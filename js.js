0756def2aab279eda2b4a76a9b02b8a506436e55
kaushika <muthuorganic@gmail.com>
2026-06-06 15:27:21 +0530

    update js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ async function show() {
 
     let x = document.querySelector("#city").value;
 
-    let apikey = "abc";
+    let apikey = "434504dde5451730f015caa41a0a44ba";
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${x}&appid=${apikey}&units=metric`;
 
@@ -19,7 +19,7 @@ async function show() {
 
     document.getElementById("res").innerHTML = `
         <h3>${data.name}</h3>
-        <p>Temperature: ${data.main.temp} °C</p>
+        <p>Temperature: ${data.main.temp}°C</p>
         <p>Weather: ${data.weather[0].description}</p>
         <p>Humidity: ${data.main.humidity}%</p>
     `;
